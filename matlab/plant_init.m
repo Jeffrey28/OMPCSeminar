6283725808637f7b9f961c7839d57c6b0c623578
Dominique Laurencelle <Dominique Laurencelle>
2016-06-25 02:17:15 +0200

    Added first parameters to init function

--- matlab/plant_init.m
+++ matlab/plant_init.m
@@ -4,6 +4,9 @@ function [ pars ] = plant_init()
 
 pars.Ts = 0; % time step in seconds TODO: Choose a meaningful value
 pars.x0 = []; % some initial state of the system TODO: Choose wisely :-)
+pars.m = 2000; % Car's mass
+pars.a = 1; % Distance of front wheels to gravity center
+pars.b = 1; % Distance of rear wheels to gravity center
 
 % test commit
 
