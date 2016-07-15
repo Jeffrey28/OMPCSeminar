010762703ac2870c87248ae396405e23aaf6e473
Maths1993 <dominique.laurencelle@hotmail.de>
2016-07-16 00:12:35 +0200

    Added linearization for state and a first approach for linearization of slip angle alpha_f/r

--- matlab/test_ltv.m
+++ matlab/test_ltv.m
@@ -4,8 +4,8 @@ import casadi.*;
 
 Ts = 0.05;
 
-Np = 6;    %Prediction horizon
-Nc = 10;     %Control horizon
+Np = 50;    %Prediction horizon
+Nc = 50;     %Control horizon
 nx = 6;     %State dimension
 nu = 1;     %Control dimension
 
@@ -16,11 +16,18 @@ m = 2050;
 I = 3344;
 g = 9.81;
 
-ui = SX.sym('u', nu);
+u = SX.sym('u', nu);
 xi = SX.sym('xi', nx);
+% xi:
+% xi(1) := x_dot
+% xi(2) := y_dot
+% xi(3) := psi
+% xi(4) := psi_dot
+% xi(5) := X
+% xi(6) := Y
 
 % System dynamics
-delta_f = ui * pi / 180;
+delta_f = u * pi / 180;
 delta_r = 0;
 
 vx_f = xi(1);
@@ -30,8 +37,12 @@ vy_r = xi(2) - b * xi(4);
 
 vl_f = vy_f * sin(delta_f) + vx_f * cos(delta_f);   % u corresponds to front steering angle
 vc_f = vy_f * cos(delta_f) - vx_f * sin(delta_f);
-vl_r = vx_r;                            % rear steering angle = 0 --> simplification
+vl_r = vx_r; % rear steering angle = 0 --> simplification
 vc_r = vy_r;
+% vl_f = vy_f * sin(delta_f) + vx_f * cos(delta_f);
+% vc_f = vy_f * cos(delta_f) - vx_f * sin(delta_f);
+% vl_r = vy_r * sin(delta_r) + vx_r * cos(delta_r);
+% vc_r = vy_r * cos(delta_r) - vx_r * sin(delta_r);
 
 alpha_f = atan(vc_f / vl_f);
 alpha_r = atan(vc_r / vl_r);
@@ -47,8 +58,12 @@ mu = 0;
 
 Fx_f = Fl_f * cos(delta_f) - Fc_f * sin(delta_f);   % u corresponds to front steering angle
 Fy_f = Fl_f * sin(delta_f) + Fc_f * cos(delta_f);   % u corresponds to front steering angle
-Fx_r = Fl_r;                            % rear steering angle = 0 --> simplification
-Fy_r = Fc_r;                            % rear steering angle = 0 --> simplification
+Fx_r = Fl_r; % rear steering angle = 0 --> simplification
+Fy_r = Fc_r; % rear steering angle = 0 --> simplification
+%Fx_f = Fl_f * cos(delta_f) - Fc_f * sin(delta_f);
+%Fy_f = Fl_f * sin(delta_f) + Fc_f * cos(delta_f);
+%Fx_r = Fl_r * cos(delta_r) - Fc_r * sin(delta_r);
+%Fy_r = Fl_r * sin(delta_r) + Fc_r * cos(delta_r);
 
 xdot = [xi(2) * xi(4) + (2 / m) * (Fx_f + Fx_r);
         -xi(1) * xi(4) + (2 / m) * (Fy_f + Fy_r);
@@ -56,13 +71,13 @@ xdot = [xi(2) * xi(4) + (2 / m) * (Fx_f + Fx_r);
         (2 / I) * (a * Fy_f - b * Fy_r);
         xi(1) * cos(xi(3)) - xi(2) * sin(xi(3));
         xi(1) * sin(xi(3)) + xi(2) * cos(xi(3))];
-    
+
 %l = ...        % Maybe add a cost function to RK4
 
-f = Function('f', {xi, ui}, {xdot});
+f = Function('f', {xi, u}, {xdot});
 
 % RK4 integrator
-Mrk = 10;
+Mrk = 20;
 U   = MX.sym('U');
 X0  = MX.sym('X0', nx);
 X = X0;
@@ -78,179 +93,142 @@ end
 
 RK4 = Function('RK4', {X0, U}, {X});
 
-% dk1_x = jacobian(k1, X0);
-% dk2_x = jacobian(k2, X0);
-% dk3_x = jacobian(k3, X0);
-% dk4_x = jacobian(k4, X0);
-% 
-% dk1_x_F = Function('dk1_x',{X0, U}, {dk1_x});
-% dk2_x_F = Function('dk2_x',{X0, U}, {dk2_x});
-% dk3_x_F = Function('dk3_x',{X0, U}, {dk3_x});
-% dk4_x_F = Function('dk4_x',{X0, U}, {dk4_x});
-% 
-% dk1_u = jacobian(k1, U);
-% dk2_u = jacobian(k2, U);
-% dk3_u = jacobian(k3, U);
-% dk4_u = jacobian(k4, U);
-% 
-% dk1_u_F = Function('dk1_u',{X0, U}, {dk1_u});
-% dk2_u_F = Function('dk2_u',{X0, U}, {dk2_u});
-% dk3_u_F = Function('dk3_u',{X0, U}, {dk3_u});
-% dk4_u_F = Function('dk4_u',{X0, U}, {dk4_u});
-% 
-% dg_x = eye(6) + (DT / 6) * (dk1_x + 2 * dk2_x + 2 * dk3_x + dk4_x);
-% dg_u = (DT / 6) * (dk1_u + 2 * dk2_u+ 2 * dk3_u + dk4_u);
-% 
-% dg_x_F = Function('dg_x', {X0, U}, {dg_x});
-% dg_u_F = Function('dg_u', {X0, U}, {dg_u});
-% 
-% xSet = X;
-% uSet = 1;
-% 
-% JacX = jacobian(X, X0);
-% JacXF = Function('XF',{X0, U}, {JacX});
-% JacU = jacobian(X, U);
-% JacXU = Function('XF',{X0, U}, {JacU});
-% %disp(JacXF(xSet, uSet));
-% disp(JacXU(xSet, uSet));
-% disp(dg_u_F(xSet, uSet));
-% %disp(dg_x_F(xSet, uSet));
+X_0 = [5;0;0;0;0;1];
+U_init = 0;
 
-% for k = 1 : Np
-%   % X = RK4(xSet, uSet) + dg_u_F(xSet, uSet) * (X - xSet) + dg_x_F(xSet, uSet) * (u(k) - uSet);
-%    X = RK4(xSet, uSet) + dg_x_F(xSet, uSet) * (X - xSet) + dg_u_F(xSet, uSet) * (u(k) - uSet); 
-%    J = J + 500*X(6)^2;
-%   % J = J + X(4)^2;
-%   % J = J + X(3)^2;
-%    if k == 1
-%        J = J + (500 * pi / 180) * (u(k)-uSet)^2;
-%    else
-%        J = J + (500 * pi / 180) * (u(k)-u(k-1))^2;
-%    end
-% end
-
-
-JacX = jacobian(X, X0);
-JacXF = Function('XF',{X0, U}, {JacX});
-JacU = jacobian(X, U);
-JacUF = Function('UF',{X0, U}, {JacU});
-
-% Formulate NLP
-u = SX.sym('u',Np);
-
-x_0 = [5;0;0;0;0;0.01];
-X = x_0;
-
-J = 0;
-g=[];
 
 % Linearization
-X = RK4(X, u(1));
-xSet = X;
-uSet = u(1);
-offset = RK4(xSet, uSet);
 
-for k = 2 : Np
-   X = offset + JacXF(xSet, uSet) * (X - xSet) + JacUF(xSet, uSet) * (u(k) - uSet);
-   J = J + X(6)^2;
-%    J = J + X(4)^2;
-%    J = J + X(3)^2;
-%    if k == 1
-%        J = J + (500 * pi / 180) * (u(k))^2;
-%    else
-%        J = J + (500 * pi / 180) * (u(k))^2;
-%    end
-end
+% For state vector
+jacobiX_state = jacobian(X, X0);
+jacobiXF_state = Function('jacobiXF_state', {X0, U}, {jacobiX_state});
+jacobiU_state = jacobian(X, U);
+jacobiUF_state = Function('jacobiUF_state', {X0, U}, {jacobiU_state});
+A = jacobiXF_state(X_0, U_init);
+B = jacobiUF_state(X_0, U_init);
 
-% % Objective function
-% J=0;
-% g=[];
-% X = RK4(X, u(1));
-% for i = 2:Np
-%     % build the cost of the NLP
-%     % 1 - get x_next using RK4
-%     % 2 - J = J + ...
-%     X = RK4(X, u(i));
-%     %J = J + 500 * X(3)^2;
-%     %J = J + 75 * (X(6) - 0.5)^2;
-%     J = J + 750 * X(6)^2;
-%     J = J + (150 * pi / 180) * (u(i-1) - u(i))^2;
-% end
+% For spip angle
+% Front angle
+jacobiX_front = jacobian(alpha_f, xi);
+jacobiXF_front = Function('jacobiFX', {xi, u}, {jacobiX_front});
+jacobiU_front = jacobian(alpha_f, u);
+jacobiUF_front = Function('jacobiUX', {xi, u}, {jacobiU_front});
+C_front = jacobiXF_front(X_0, U_init);
+D_front = jacobiUF_front(X_0, U_init);
+% Rear angle
+jacobiX_rear = jacobian(alpha_r, xi);
+jacobiXF_rear = Function('jacobiFX', {xi, u}, {jacobiX_rear});
+jacobiU_rear = jacobian(alpha_r, u);
+jacobiUF_rear = Function('jacobiUX', {xi, u}, {jacobiU_rear});
+C_rear = jacobiXF_rear(X_0, U_init);
+D_rear = jacobiUF_rear(X_0, U_init);
+%offset = RK4(X_0, U_init);
 
 
-% testN = 5000;
-% 
-% x_test = zeros(testN);
-% y_test = zeros(testN);
-% 
-% Fk = x_0;
-% myU = -10;
-% for i=0:testN-1
-%     Fk = RK4(Fk, myU);
-%     Fk = full(Fk);
-%     x_test(i+1) = Fk(5);
-%     y_test(i+1) = Fk(6);
-%     if i==testN/2
-%        myU = -5; 
-%     end
-% end
-% disp([x_test(1), y_test(1)]);
-% disp([x_test(testN), y_test(testN)]);
-% 
-% plot(x_test, y_test); hold on;
-
-
-% Terminal constraints: x_0(T)=x_1(T)=0
-%g = [g; X];
-
-% Allocate an NLP solver
-nlp = struct('x', u, 'f', J, 'g', g);
-
-% Create IPOPT solver object
-options = struct;
-%options.hessian_approximation = 'limited-memory';
-solver = nlpsol('solver', 'ipopt', nlp);
-
-% arg = struct;
-% % YOUR CODE HERE: upper and lower bounds on x and g(x)
-% arg.x0  =  zeros(1, Np);        % solution guess
-% arg.lbx =  -inf;       % lower bound on x
-% arg.ubx =  inf;       % upper bound on x
-% % arg.lbg =  [];       % lower bound on g
-% % arg.ubg =  [];       % upper bound on g
-
+% Start with an empty NLP
+w={};
 w0 = [];
 lbw = [];
 ubw = [];
+J = 0;
+g={};
+lbg = [];
+ubg = [];
 
-for i=1:Np
-    w0 = [w0, 2];
-    lbw = [lbw, 10];
-    ubw = [ubw, 10];
+% "Lift" initial conditions
+U_prev = MX.sym('U_prev', 1);
+X0 = MX.sym('X0', 6);
+w = {w{:}, U_prev, X0};
+lbw = [lbw; U_init; X_0];
+ubw = [ubw; U_init; X_0];
+w0 = [w0; U_init; X_0];
+
+% Formulate the NLP
+Xk = X0;
+for k=0:Np
+    % New NLP variable for the control
+    Uk = MX.sym(['U_' num2str(k)]);
+    DUk = MX.sym(['DU_' num2str(k)]);
+    w = {w{:}, Uk, DUk};
+    
+    if k < Nc
+        lbw = [lbw; -10; -1.5];
+        ubw = [ubw;  10;  1.5];
+        w0 = [w0;  0;  0];
+    else
+        lbw = [lbw; -10; 0];
+        ubw = [ubw;  10; 0];
+        w0 = [w0;  0;  0];
+    end
+
+    % Add action constraint
+    g = {g{:}, U_prev - (Uk + DUk)};
+    lbg = [lbg; 0];
+    ubg = [ubg; 0];
+    U_prev = Uk;
+    
+
+    % Integrate till the end of the interval
+    
+    % State linearization
+    Xk_end = A * Xk + B * Uk;
+    %Xk_end = offset + A * (Xk-X_0) + B * (Uk-U_init);
+    
+    % Slip angle linearization
+    % Front
+    alpha_fk_end = C_front * Xk + D_front * Uk;
+    % Rear
+    alpha_rk_end = C_rear * Xk + D_rear * Uk;
+    
+    %Xk_end = Fk.xf;
+    %J=J+Fk.qf;
+    J = J + 1750 * Xk_end(6) * Xk_end(6);
+    %J = J + 500 * Xk_end(3) * Xk_end(3);
+    J = J + 150 * (pi / 180) * DUk * DUk;
+    %% TODO: Add soft-constraint for alpha %% 
+
+    % New NLP variable for state at end of interval
+    Xk = MX.sym(['X_' num2str(k+1)], 6);
+    w = {w{:}, Xk};
+    lbw = [lbw; -inf; -inf; -inf; -inf; -inf; -inf];
+    ubw = [ubw;  inf;  inf;  inf;  inf;  inf;  inf];
+    w0 = [w0; 5; 0; 0; 0; 0; 0];
+
+    % Add equality constraint for state
+    g = {g{:}, Xk_end-Xk};
+    lbg = [lbg; 0; 0; 0; 0; 0; 0];
+    ubg = [ubg; 0; 0; 0; 0; 0; 0];
 end
 
-%% Solve the NLP
-res = solver('x0', w0, 'lbx', lbw, 'ubx', ubw);
+% Create an NLP solver
+prob = struct('f', J, 'x', vertcat(w{:}), 'g', vertcat(g{:}));
+solver = nlpsol('solver', 'ipopt', prob);
 
-f_opt       = full(res.f);
-u_opt       = full(res.x);
+sol = solver('x0', w0, 'lbx', lbw, 'ubx', ubw, 'lbg', lbg, 'ubg', ubg);
 
-xNext = x_0;
+f_opt       = full(sol.f);
+u_opt       = full(sol.x);
+u_opt       = u_opt(8:8:end);
+
+xNext = X_0;
 pars = plant_init();
 
 for k=0:Np-1
     xNext = plant_step(xNext, u_opt(k+1), Ts, k, pars);
-    res1(k+1, :) = xNext;
+    res(k+1, :) = xNext;
     %disp(xNext);
 end
 
-res_Dx = res1(:, 1);
-res_Dy = res1(:, 2);
-res_p = res1(:, 3);
-res_Dp = res1(:, 4);
-res_X = res1(:, 5);
-res_Y = res1(:, 6);
+res_Dx = res(:, 1);
+res_Dy = res(:, 2);
+res_p = res(:, 3);
+res_Dp = res(:, 4);
+res_X = res(:, 5);
+res_Y = res(:, 6);
 
 
-% Plot results
-plot_result(u_opt,res1);
+% % Plot results
+% plot(u_opt); hold on;
+% plot(res_X, res_Y); hold on;
+% axis equal;
+plot_result( u_opt, res );
