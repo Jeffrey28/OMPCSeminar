503fc96bde2d2cfdc9950ec16566ac3cf2d72342
Stefan Glaser <stefan.glaser@hs-offenburg.de>
2016-07-17 18:45:26 +0200

    Added new version of result plotter.

--- matlab/plot_result.m
+++ matlab/plot_result.m
@@ -1,11 +1,20 @@
 function [ ] = plot_result( u_opt, res )
 %PLOT_RESULT Plot the result
 
-figure;
-subplot(2,1,1);
+size = length(res(:, 1));
 
-% Plot car trajectory
+figure;
+subplot(3,1,1);
+
+% Plot car XY-trajectory
+hold on;
 plot(res(:, 5), res(:, 6));
+y_trajectory = zeros(size, 1);
+for i=1:size
+    y_trajectory(i) = YTrajectory(res(i, 5));
+end
+plot(res(:, 5), y_trajectory, '--g');
+
 
 grid on;
 axis equal;
@@ -13,7 +22,23 @@ xlabel('x (m)'); ylabel('y (m)');
 %axis([0 2 -1 1]);
 title('Car trajectory');
 
-subplot(2,1,2);
+% Car angle trajectory
+subplot(3,1,2);
+hold on;
+plot(res(:, 5), res(:, 3));
+psi_trajectory = zeros(size, 1);
+for i=1:size
+    psi_trajectory(i) = PsiTrajectory(res(i, 5));
+end
+plot(res(:, 5), psi_trajectory, '--g');
+
+grid on;
+xlabel('x (m)'); ylabel('psi (deg)');
+title('Car angle trajectory');
+
+
+% Plot steering actions
+subplot(3,1,3);
 plot(u_opt);
 
 grid on;
