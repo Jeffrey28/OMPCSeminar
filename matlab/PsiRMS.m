2ef6168787a81da2972af28da08d304388c8f774
Maths1993 <dominique.laurencelle@hotmail.de>
2016-07-18 16:54:37 +0200

    Added RMS functions for Y and Psi

---
+++ matlab/PsiRMS.m
@@ -0,0 +1,19 @@
+function [ PsiRMS ] = PsiRMS( res )
+
+X_sim = res(:, 5);
+Psi_sim = res(:, 3) * (180 / pi);
+
+size = length(res);
+
+Psi_ref = zeros(size, 1);
+
+for i = 1 : size
+    Psi_ref(i) = (180 /pi) * PsiTrajectory(X_sim(i));
+end
+
+Psi_diff = Psi_sim - Psi_ref;
+sqDiff = Psi_diff.^2;
+PsiRMS = sum(sqDiff) / size;
+
+end
+
---
+++ matlab/YRMS.m
@@ -0,0 +1,18 @@
+function [ YRMS ] = YRMS( res )
+
+X_sim = res(:, 5);
+Y_sim = res(:, 6);
+
+size = length(res);
+
+Y_ref = zeros(size, 1);
+
+for i = 1 : size
+    Y_ref(i) = YTrajectory(X_sim(i));
+end
+
+Y_diff = Y_sim - Y_ref;
+sqDiff = Y_diff.^2;
+YRMS = sum(sqDiff) / size;
+
+end
